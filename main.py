83a25672055a2372b45f4987136c182bbeda6c67
haoqin <hqin2@ualberta.ca>
2020-02-27 03:46:00 -0700

    test

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import sqlite3
+import sys
+
+# ********Initialization********
+def conn_db():
+    conn = sqlite3.connect(sys.argv[1])
+    cursor = conn.cursor()
+    return cursor
+
 if __name__ == "__main__":
-    print("test")
+    cursor = conn_db()
+
