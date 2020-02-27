b6adcc220978939636eecee6237bdf3d162c546f
haoqin <hqin2@ualberta.ca>
2020-02-27 04:06:59 -0700

    commit

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ def conn_db():
     cursor = conn.cursor()
     return cursor
 
+# ********Registered User Login********
+def reg_login(cursor, email, password):
+    cursor.execute('SELECT * FROM users WHERE users.email = ? AND users.pwd = ?', (email, password))
+    if cursor.fetchall():
+        return 1
+    return 0
+
+
 if __name__ == "__main__":
     cursor = conn_db()
 
+    # ********Registered User Login Test********
+    value1 = reg_login(cursor, 'dne@dne.com', 'password')
+    print(value1)
+    value2 = reg_login(cursor, 'ibev@gmail.com', 'password')
+    print(value2)
+    value3 = reg_login(cursor, 'rupertd@yahoo.ca', 'wrong')
+    print(value3)
