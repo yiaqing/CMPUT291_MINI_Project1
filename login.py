de21ae7d88bda051fe80df9c2529101096e5a36c
haoqin <hqin2@ualberta.ca>
2020-02-28 01:02:34 -0700

    commit

--- login.py
+++ login.py
@@ -3,6 +3,16 @@ import sqlite3
 
 # ********Registered User Login********
 def reg_login(cursor, email, password):
+    email = email.lower()
+
+    if len(email) > 20:
+        print("email out of bounds")
+        return 0
+
+    if len(password) > 4:
+        print("password out of bounds")
+        return 0
+
     cursor.execute('SELECT * FROM users WHERE users.email = ? AND users.pwd = ?;', (email, password))
     if cursor.fetchall():
         return email
@@ -11,6 +21,35 @@ def reg_login(cursor, email, password):
 
 # ********Unregistered User Sing up********
 def sign_up(conn, cursor, email, name, pwd, city, gender):
+    email = email.lower()
+    name = name.title()
+    city = city.capitalize()
+    gender = gender.upper()
+
+    if len(email) > 20:
+        print("email out of bounds")
+        return 0
+
+    if len(name) > 16:
+        print("name out of bounds")
+        return 0
+
+    if len(pwd) > 4:
+        print("password out of bounds")
+        return 0
+
+    if len(city) > 15:
+        print("city out of bounds")
+        return 0
+
+    if len(gender) > 1:
+        print("gender out of bounds")
+        return 0
+
+    if (gender != "M") and (gender != "F"):
+        print("gender out of bounds")
+        return 0
+
     cursor.execute('INSERT INTO users (email, name, pwd, city, gender) VALUES(?, ?, ?, ?, ?);',
                    (email, name, pwd, city, gender))
     conn.commit()
--- main.py
+++ main.py
@@ -22,33 +22,39 @@ if __name__ == "__main__":
     cursor = conn.cursor()
 
     # ********Registered User Login Test********
-    value1 = login.reg_login(cursor, 'dne@dne.com', 'password')
-    print(value1)
-    value2 = login.reg_login(cursor, 'ibev@gmail.com', 'password')
-    print(value2)
-    value3 = login.reg_login(cursor, 'rupertd@yahoo.ca', 'wrong')
-    print(value3)
+    # value1 = login.reg_login(cursor, 'dne@dne.com', 'password')
+    # print(value1)
+    # value2 = login.reg_login(cursor, 'ibev@gmail.com', 'password')
+    # print(value2)
+    # value3 = login.reg_login(cursor, 'rupertd@yahoo.ca', 'wrong')
+    # print(value3)
 
     # ********User Sign up Test********
-    # login.sign_up(conn, cursor, 't3@t2.com', 't123', 't123', 'Edmonton', 'M')
+    login.sign_up(conn, cursor, 'abcdefghijklmn@t2.com', 't123', 't123', 'Edmonton', 'M')
+    login.sign_up(conn, cursor, 'abcdefghijklm@t2.com', 't123', 't123', 'Edmonton', 'A')
+    login.sign_up(conn, cursor, 'abcdefghijklm@t2.com', 't12344', 't12444443', 'Edmonton', 'F')
+
+    login.sign_up(conn, cursor, 'abcdefghijklm@t5.com', 't123', 't123', 'Edmonton', 'M')
 
     # ********Listing Products Test********
-    value = list_products.list_products(cursor)
-    for i in range(0, len(value)):
-        print(value[i])
+    # value = list_products.list_products(cursor)
+    # for i in range(0, len(value)):
+    #     print(value[i])
 
     # ********Write product review test********
-    list_products.write_preview(conn, cursor, 'G11', 'ibev@gmail.com', '5', 'test')
-    list_products.write_preview(conn, cursor, 'G01', 'ibev@gmail.com', '5', 'testtesttesttesttesta')
-    list_products.write_preview(conn, cursor, 'G01', 'ibev@gmail.com', '-1', 'test')
-    list_products.write_preview(conn, cursor, 'G01', 'ibev@gmail.com', '6', 'test')
+    # list_products.write_preview(conn, cursor, 'G11', 'ibev@gmail.com', '5', 'test')
+    # list_products.write_preview(conn, cursor, 'G01', 'ibev@gmail.com', '5', 'testtesttesttesttesta')
+    # list_products.write_preview(conn, cursor, 'G01', 'ibev@gmail.com', '-1', 'test')
+    # list_products.write_preview(conn, cursor, 'G01', 'ibev@gmail.com', '6', 'test')
 
-    list_products.write_preview(conn, cursor, 'G01', 'ibev@gmail.com', '5', 'test')
+    # list_products.write_preview(conn, cursor, 'G01', 'ibev@gmail.com', '5', 'test')
+    # list_products.write_preview(conn, cursor, 'g01', 'IBEV@gmail.com', '5', 'test')
 
     # ********List reviews test********
-    list_products.list_reviews(cursor, 'G10000')
-    list_products.list_reviews(cursor, 'G11')
-    list_products.list_reviews(cursor, 'G01')
+    # list_products.list_reviews(cursor, 'g10000')
+    # list_products.list_reviews(cursor, 'g11')
+    # list_products.list_reviews(cursor, 'G01')
+    # list_products.list_reviews(cursor, 'g01')
 
     # ********Disconnect Database********
     disconn_db(conn, cursor)
