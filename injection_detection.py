fe6ebc3b46a3bf365398bc32a422b3e7c6210449
haoqin <hqin2@ualberta.ca>
2020-03-01 04:19:51 -0700

    commit

--- injection_detection.py
+++ injection_detection.py
@@ -1,8 +1,9 @@
 import re
 
+
 # ********Search SQL Metadata********
 def search(string):
     if re.search(r"\b(and|exec|insert|select|drop|grant|alter|delete"
-              r"|update|count|chr|mid|master|truncate|char|declare|or)\b|(\*|;|\+|'|%)", string) is not None:
+                 r"|update|count|chr|mid|master|truncate|char|declare|or)\b|(\*|;|\+|'|%)", string) is not None:
         print("Injection")
         return 1
